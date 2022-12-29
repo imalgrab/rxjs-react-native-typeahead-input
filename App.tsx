8dc33807e15de146e6e1be93e7bf1293beb074d2
imalgrab <igormalgrab@gmail.com>
2022-12-29 15:52:32 +0100

    add rxjs and setup first observable

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { interval, Observer } from 'rxjs';
+
+const value$ = interval(1000);
+
+const observer: Partial<Observer<number>> = {
+  next: (v) => console.log(v),
+};
 
 export default function App() {
+  const [label, setLabel] = useState<number>();
+
+  useEffect(() => {
+    if (value$) {
+      const subscription = value$.subscribe((value) => setLabel(value));
+      return () => subscription.unsubscribe();
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{label}</Text>
       <StatusBar style="auto" />
     </View>
   );
